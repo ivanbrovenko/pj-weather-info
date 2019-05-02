07ed50763b65f89b4f37c232d39ddf48a3194d4d
Ivan_Brovenko <ivan.brovenko@epam.com>
2019-05-03 00:21:05 +0300

    added heroku port

--- back-end/app.js
+++ back-end/app.js
@@ -50,4 +50,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, () => console.log('http://localhost:3000'));
+app.listen(process.env.PORT, () => console.log('http://localhost:3000'));
